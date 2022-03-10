376b7953c2d3720860e6973434fabd2f553c6c76
unknown <markosortega15@gmail.com>
2022-03-10 14:21:30 -0500

    entrega de la interfaz grafica del proyecto de parqueadero

--- src/main/java/com/einsteincolombia/parqueaderoscrum/controller/ParqueaderoController.java
+++ src/main/java/com/einsteincolombia/parqueaderoscrum/controller/ParqueaderoController.java
@@ -1,14 +1,42 @@
 package com.einsteincolombia.parqueaderoscrum.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
-public class ParqueaderoController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+public class ParqueaderoController implements Initializable {
+
+    @FXML private TextField textPlaca;
+    @FXML private TextField textModelo;
+    @FXML private TextField textFecha;
+    @FXML private TextField textCantMotos;
+    @FXML private TextField textCantCarros;
+    @FXML private ComboBox<String> comboBoxTipo;
+    @FXML private Button buttonAgregar;
+    @FXML private Button buttonLimpiar;
+    @FXML private Button buttonReiniciar;
+    @FXML private Button buttonTotalizar;
+    @FXML private Label labelResultado;
+
+    ObservableList<String> comboTipoContent =
+            FXCollections.observableArrayList(
+                    "Moto",
+                    "Carro"
+            );
+
+    public ParqueaderoController() {
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        comboBoxTipo.setItems(comboTipoContent);
     }
 }
